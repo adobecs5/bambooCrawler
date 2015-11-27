3cc34f099a1fa4a8590d969dcab8c8e45b86f058
Cheolho Jeon <adobecs5.kaist@gmail.com>
2015-11-27 21:55:07 +0900

    nov 27 - 2

--- main.py
+++ main.py
@@ -2,38 +2,50 @@ from bs4 import BeautifulSoup as bs
 import urllib
 import http.cookiejar
 
-cj = http.cookiejar.LWPCookieJar()
-opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-urllib.request.install_opener(opener)
+def login_and_return_opener():
+    cj = http.cookiejar.LWPCookieJar()
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+    urllib.request.install_opener(opener)
 
-homepage_url = "https://bamboofo.rest/users/sign_in"
-user_id = "download@kaist.ac.kr"
-user_pass = "00000000"
-a = opener.open(homepage_url)
-soup = bs(a, "html.parser")
-CSRF_token = str(soup.find(attrs={"name": "csrf-token"})).split(" ")[1].split("=")[1][1:] + "="
+    homepage_url = "https://bamboofo.rest/users/sign_in"
+    user_id = "download@kaist.ac.kr"
+    user_pass = "00000000"
+    a = opener.open(homepage_url)
+    soup = bs(a, "html.parser")
+    CSRF_token = str(soup.find(attrs={"name": "csrf-token"})).split(" ")[1].split("=")[1][1:] + "="
 
-params = urllib.parse.urlencode({"utf-8":"✓", "authenticity_token": CSRF_token,"user[email]":user_id, "user[password]":user_pass})
-params = params.encode('utf-8')
-req = urllib.request.Request(homepage_url, params)
-res = opener.open(req)
-page = "https://bamboofo.rest/posts"
-page = opener.open("https://bamboofo.rest/posts")
-soup = bs(page, "html.parser")
-# print(soup)
-new_posts = soup.findAll("tr", { "class" : "row_type_N" }) # get New posts
-links = []
-for post in new_posts:
-    links.append(post.attrs['href'].split("/")[-1])
-updated_posts = soup.findAll("tr", { "class" : "row_type_U" }) # get Updated posts
-for post in updated_posts:
-    links.append(post.attrs['href'].split("/")[-1])
+    params = urllib.parse.urlencode({"utf-8":"✓", "authenticity_token": CSRF_token,"user[email]":user_id, "user[password]":user_pass})
+    params = params.encode('utf-8')
+    req = urllib.request.Request(homepage_url, params)
+    res = opener.open(req)
+    return opener
 
-aLink = links[0]
-full_url = "https://bamboofo.rest/posts/" + aLink
-req = urllib.request.Request(full_url)
-res = opener.open(req)
-soup = bs(res, "html.parser")
+def get_new_posts(opener):
+    page = "https://bamboofo.rest/posts"
+    page = opener.open("https://bamboofo.rest/posts")
+    soup = bs(page, "html.parser")
+    # print(soup)
+    new_posts = soup.findAll("tr", { "class" : "row_type_N" }) # get New posts
+    links = []
+    for post in new_posts:
+        links.append(post.attrs['href'].split("/")[-1])
+    return links
+
+def get_updated_posts(opener):
+    links = []
+    page = "https://bamboofo.rest/posts"
+    page = opener.open("https://bamboofo.rest/posts")
+    soup = bs(page, "html.parser")
+    updated_posts = soup.findAll("tr", { "class" : "row_type_U" }) # get Updated posts
+    for post in updated_posts:
+        links.append(post.attrs['href'].split("/")[-1])
+    return links
+
+def get_post(opener, aLink):
+    full_url = "https://bamboofo.rest/posts/" + aLink
+    req = urllib.request.Request(full_url)
+    res = opener.open(req)
+    soup = bs(res, "html.parser")
 
 class post(object):
     def __init__(self, post, comments):
@@ -83,5 +95,10 @@ def get_texts(soup):
 
 #comment = soup.findAll("div", {"class" : "col-md-3"})
 #print(comment[0].text.replace(" ", "").replace("\n", ""))
-a = get_texts(soup)
-print (a)
+if __name__ == "main":
+    opener = login_and_return_opener()
+    page = "https://bamboofo.rest/posts"
+    page = opener.open("https://bamboofo.rest/posts")
+    soup = bs(page, "html.parser")
+    a = get_texts(soup)
+    print (a)
